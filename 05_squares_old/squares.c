6c952122076f79b2d4cfc732318e641d75663044
Coursera Learner <nobody@nowhere.nul>
2020-06-10 05:35:51 +0000

    Thanks

--- 05_squares_old/squares.c
+++ 05_squares_old/squares.c
@@ -30,8 +30,7 @@ int isAtBorder(int coord, int offset, int size) {
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-  int w=0;
-  int h=0;
+  int w, h;
   if(size1 > (x_offset + size2)){
     w = size1;
   } else {
@@ -65,7 +64,7 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
 	 (((y>=y_offset) && (y<y_offset+size2)) && ((x=x_offset) || (x=x_offset+size2-1)))){
 	printf("*");
       } else{
-	if(((x<size1) && ((y=0) || (y=size1-1))) || ((y<size1) && ((x=0) || (x=size1-1)))){
+	if(((x<size1 && (y=0 || (y=size1-1))) || (y<size1 && ((x=0) || (x=size1-1)))){
 	  printf("#");
 	} else {
 	  printf(" ");
