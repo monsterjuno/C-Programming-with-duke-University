5148d361d152b6b2c93d724fa3795325e0393feb
Coursera Learner <nobody@nowhere.nul>
2020-06-21 13:28:43 +0000

    sdnackbxfhj

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include "deck.h"
 #include "cards.h"
+
 void print_hand(deck_t * hand){
   size_t i=0;
   print_card(*hand->cards[i++]);
@@ -10,7 +11,6 @@ void print_hand(deck_t * hand){
     printf(" ");
     print_card(*hand->cards[i++]);
   }
-  /*printf(" ");*/
 }
 
 int deck_contains(deck_t * d, card_t c) {
@@ -55,72 +55,5 @@ void assert_full_deck(deck_t * d) {
       assert(rep[i]>1);
     }
   }
-}
-
-void add_card_to(deck_t * deck, card_t c){
-  deck->n_cards++;
-  deck->cards = realloc(deck->cards,(deck->n_cards)*sizeof(*(deck->cards)));
-  deck->cards[deck->n_cards-1] = NULL;
-  deck->cards[deck->n_cards-1] = realloc(deck->cards[deck->n_cards-1], sizeof(*(deck->cards[deck->n_cards-1])));
-  deck->cards[deck->n_cards-1]->suit = c.suit;
-  deck->cards[deck->n_cards-1]->value = c.value;
-}
-
-card_t * add_empty_card(deck_t * deck){
-  card_t * temp = malloc(sizeof(*temp));
-  temp->suit = 0;
-  temp->value = 0;
-  deck->n_cards++;
-  deck->cards = realloc(deck->cards, (deck->n_cards)*sizeof(*(deck->cards)));
-  deck->cards[deck->n_cards-1] = temp;
-  return temp;
-}
-
-deck_t * make_deck_exclude(deck_t * excluded_cards){
-  card_t temp;
-  deck_t * res = malloc(sizeof(*res));
-  res->n_cards = 0;
-  res->cards = NULL;
-
-  int cond = 0;
-  for(unsigned i=0; i<52; i++){
-    temp = card_from_num(i);
-    cond = deck_contains(excluded_cards, temp);
-    if(cond==0){
-      add_card_to(res, temp);
-    }
-  }
-  return res;
-}
-
-deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands){
-  deck_t * temp;
-  deck_t * no_ret_d = malloc(sizeof(*no_ret_d));
-  deck_t * ret_d;
-  no_ret_d->n_cards = 0;
-  no_ret_d->cards = NULL;
-  card_t * temp_c;
-  for(size_t i=0; i<n_hands; i++){
-    temp = hands[i];
-    for(size_t j=0; j<temp->n_cards; j++){
-      temp_c = temp->cards[j];
-      if(temp_c->value != 0){
-	if(deck_contains(no_ret_d, *temp_c)==0){
-	  add_card_to(no_ret_d, *temp_c);
-	}
-      }
-    }
-  }
-  ret_d = make_deck_exclude(no_ret_d);
-  free_deck(no_ret_d);
-  return ret_d;
-}
-
-void free_deck(deck_t * deck){
-  for(size_t i=0; i<deck->n_cards; i++){
-    free(deck->cards[i]);
-  }
-  free(deck->cards);
-  free(deck);
 }
 
